b7964f039d3bd98ba3920ce5cbdc44cd765cd27a
Mateus Rodrigues <mateusrvdev@gmail.com>
2024-02-09 15:19:42 -0300

    Ĩmplementação de variáveis de ambiente.

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,8 +1,13 @@
+require("dotenv/config");
 const { mongoose } = require("mongoose");
 
+const { DB_USER_NAME, DB_DATABASE_NAME, DB_PASSWORD, DB_RETRY_WRITES } =
+  process.env;
+
 mongoose.connect(
-  "mongodb+srv://mateus:Mateus0386@livraria.aynj3tq.mongodb.net/livraria"
+  `mongodb+srv://${DB_USER_NAME}:${DB_PASSWORD}@${DB_DATABASE_NAME}.mongodb.net/${DB_RETRY_WRITES}`
 );
+
 const db = mongoose.connection;
 
 module.exports = db;
